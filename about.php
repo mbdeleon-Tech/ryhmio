37107a6f756131f42624740893ba6ba94daa23cc
Marco Arsenio De Leon <mbdeleon@fit.edu.ph>
2026-07-17 12:06:44 +0800

    Update about.php

--- about.php
+++ about.php
@@ -4,18 +4,17 @@ $page_title = "About - Rhymio";
 include("include/header.php");
 ?>
 <section class="container py-5">
-    <div class="row align-items-center g-4">
-        <div class="col-lg-6">
+    <div class="row">
+        <div class="col-lg-9">
             <h1 class="section-title">About Rhymio</h1>
             <p>Rhymio is a student project company that sells musical instruments for beginners, hobbyists, and performers. The website includes buyer registration, email confirmation, categorized products, a shopping cart, checkout, a payment page, seller inventory management, and reports.</p>
             <p><strong>Group:</strong> PHPStorm</p>
             <h2 class="h4 mt-4">Group Members</h2>
             <ul>
                 <li>Marco Arsenio De Leon</li>
+                <li>Mohammad Al Sharip A. Sakaluran</li>
+                <li>Tracey Justin A. Devilleres</li>
             </ul>
-        </div>
-        <div class="col-lg-6">
-            <img class="img-fluid rounded shadow-sm" src="assets/products/stagelite-keyboard.jpg" alt="StageLite electronic keyboard sold by Rhymio">
         </div>
     </div>
 </section>

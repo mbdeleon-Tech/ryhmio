64fd01023d6a79737817326fb0f0fc50ccd5615d
Marco Arsenio De Leon <mbdeleon@fit.edu.ph>
2026-07-17 16:28:52 +0800

    Update and rename db_config.example.php to db_config.php

--- db_config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Copy this file to db_config.php and enter the database values for your server.
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "rhymio_music");
-?>
---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_HOST", "sql106.infinityfree.com");
+define("DB_USER", "if0_42361567");
+define("DB_PASSWORD", "LkrDM2mPrQsS");
+define("DB_NAME", "if0_42361567_db_rhymio");
+?>
